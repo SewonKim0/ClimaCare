9fad0e6b1b16052a6b1bfa28aaeb272459edce2a
kathytran88 <kathyngananhtran@gmail.com>
2024-03-23 17:15:49 -0400

    new

--- client/src/pages/care.jsx
+++ client/src/pages/care.jsx
@@ -1,25 +1,46 @@
 import './care.css'
 export default function HomePage() {
     return (
+<>
+<h1>How to take care of yourself in this situation</h1>
+<h2>Richmond Earthquake</h2>
+<div class='content'>
 
-
-<div className='content'>
-
-<div className='article'>
-  <h3>Create plant pots:</h3>
+<div class='article'>
+  <h3>Before</h3>
   <img src='1.png' alt='plant pot'/>
+  <h4>
+- Find a fire extinguisher <br></br>
+- Learn first aid <br></br>
+- Turn off the gas, water, and electricity <br></br>
+- Don't leave heavy objects on shelves
+  </h4>
 </div>
 
-<div className='article'>
-  <h3>Create a jewelry stand:</h3>
+<div class='article'>
+  <h3>During</h3>
   <img src='2.png' alt='jewelry stand'/>
+  <h4>
+- Stay where you are <br></br>
+- Stand against a wall near the center of the building <br></br>
+- Stay in the open away from power lines or anything that might fall <br></br>
+- Don't use matches, candles, or any flame
+  </h4>
 </div>
 
-<div className='article'>
-  <h3>Craft a Christmas tree:</h3>
+<div class='article'>
+  <h3>After</h3>
   <img src='3.png' alt='Christmas tree'/>
+  <h4>
+- Turn on the radio <br></br>
+- Stay out of damaged buildings <br></br>
+- Be careful around broken glass and debris <br></br>
+- Be careful of chimneys (they may fall on you) <br></br>
+- Stay away from beaches <br></br>
+  </h4>
 </div>
 
 </div>
+</>
     )
 }
