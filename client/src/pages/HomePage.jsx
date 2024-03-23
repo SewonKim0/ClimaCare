6c7f80d510c7f960cf7efc6476df8a46be776d82
Sewon Kim <sewonkim0black@gmail.com>
2024-03-23 18:22:58 -0400

    Homepage: form design moderately complete

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -4,18 +4,18 @@ export default function HomePage() {
   return (
     <div>
       <form method='post' action='localhost:3000/airquality'>
-        <label>
-          <p className="form-text">State</p> 
+        <label className="form-field">
+          <p>State</p> 
           <input type='text' name='state' required />
         </label>
 
-        <label>
-          <p className="form-text">City</p> 
+        <label className="form-field">
+          <p>City</p> 
           <input type='text' name='city' required/>
         </label>
 
-        <label>
-          <p className="form-text">Zip Code</p> 
+        <label className="form-field">
+          <p>Zip Code</p> 
           <input type='text' name='zip-code' required/>
         </label>
 
