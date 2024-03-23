5c01eea2f92501615d3efd0a9fa2c434388ac0ae
kathytran88 <kathyngananhtran@gmail.com>
2024-03-23 15:50:11 -0400

    form

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,31 @@
+import './homepage.css'
+
 export default function HomePage() {
   return (
     <div>
       <h1>Home Page</h1>
       <p>This is the home page.</p>
+
+      <form method='post' action='localhost:3000/airquality'>
+        <label>
+          State 
+          <input type='text' name='state' required />
+        </label>
+
+        <label>
+          City 
+          <input type='text' name='city' required/>
+        </label>
+
+        <label>
+          Zip Code
+          <input type='text' name='zip-code' required/>
+        </label>
+
+        <div>
+          <input type='submit' value='Send'/>
+        </div>
+      </form>
     </div>
   )
 }
