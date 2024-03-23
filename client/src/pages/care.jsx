68eea2d9007b1d1260cf173c46575d5ce29e2c1c
kathytran88 <kathyngananhtran@gmail.com>
2024-03-23 16:24:44 -0400

    background

---
+++ client/src/pages/care.jsx
@@ -0,0 +1,25 @@
+import './care.css'
+export default function HomePage() {
+    return (
+
+
+<div class='content'>
+
+<div class='article'>
+  <h3>Create plant pots:</h3>
+  <img src='1.png' alt='plant pot'/>
+</div>
+
+<div class='article'>
+  <h3>Create a jewelry stand:</h3>
+  <img src='2.png' alt='jewelry stand'/>
+</div>
+
+<div class='article'>
+  <h3>Craft a Christmas tree:</h3>
+  <img src='3.png' alt='Christmas tree'/>
+</div>
+
+</div>
+    )
+}
