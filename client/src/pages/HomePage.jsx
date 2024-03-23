71145ea2d9e24e8aebf4f6a309956ead450969ac
Sewon Kim <sewonkim0black@gmail.com>
2024-03-23 18:29:09 -0400

    Homepage: minor design changes for submit button of form

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,28 +3,23 @@ import './homepage.css'
 export default function HomePage() {
   return (
     <div>
-      <h1>Home Page</h1>
-      <p>This is the home page.</p>
-
       <form method='post' action='http://localhost:3000/airquality'>
-        {/* <label>
-          State 
+        <label className="form-field">
+          <p>State</p>
           <input type='text' name='state' required />
         </label>
 
         <label className="form-field">
           <p>City</p> 
           <input type='text' name='city' required/>
-        </label> */}
+        </label>
 
-        <label>
-          Zip Code
+        <label className="form-field">
+          <p>Zip Code</p>
           <input type='text' name='zipcode' required/>
         </label>
 
-        <div>
-          <input type='submit' value='Send'/>
-        </div>
+        <input className="form-submit" type='submit' value='Send'/>
       </form>
     </div>
   )
