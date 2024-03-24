5d5e42c9affd482582260093a6975a4715eb1461
kathytran88 <kathyngananhtran@gmail.com>
2024-03-24 08:55:27 -0400

    new

--- client/src/pages/care.jsx
+++ client/src/pages/care.jsx
@@ -43,22 +43,44 @@ export default function HomePage() {
 
 </div>
 
-<table>
-    <tr>
-        <th>Possible Symptoms</th>
-        <th>Suggestions</th>
-    </tr>
+<h1>Food Earthquake Toolkit by Wakefern</h1>
+<h2>As an attempt to tackle climate change impacts, Wakefern Bowl & Basket is proud to provide discounts for Earthquake and Flood victims</h2>
 
-    <tr>
-        <th>Faint/dizzy/nauseous</th>
-        <th>Visit </th>
-    </tr>
+<div className='content2'>
 
-    <tr>
-        <th>panicky/panic attacks/scared</th>
-        <th>Contact</th>
-    </tr>
-</table>
+<div className='article2'>
+  <h3>Spring water</h3>
+  <img className="article-img2" src='water.png' alt='before'/>
+  <h4>
+- Each bottle is 16 fl oz, the amount suitable for storing during an earthquake
+- Clean and crisp taste for hydrating during an earthquake
+  </h4>
+</div>
+
+<div className='article2'>
+  <h3>Dried Cranberries</h3>
+  <img className="article-img2" src='fruit.png' alt='during'/>
+  <h4>
+- Stay where you are <br></br>
+- Stand against a wall near the center of the building <br></br>
+- Stay in the open away from power lines or anything that might fall <br></br>
+- Don't use matches, candles, or any flame
+  </h4>
+</div>
+
+<div className='article2'>
+  <h3>Canned Beef</h3>
+  <img className="article-img2" src='meat.png' alt='after'/>
+  <h4>
+- Turn on the radio <br></br>
+- Stay out of damaged buildings <br></br>
+- Be careful around broken glass and debris <br></br>
+- Be careful of chimneys (they may fall on you) <br></br>
+- Stay away from beaches <br></br>
+  </h4>
+</div>
+
+</div>
 
 <footer>
           <div className='col1'>
